f1e1d4747d44b5a728b2fb6d17561cefd52953cb
Thiago Rocha <98711998+Dithan@users.noreply.github.com>
2025-02-08 22:01:22 -0300

    Feat: Estrutura/header/apresentacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/estrutura/header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ApresentacaoComponent } from './componentes/home/apresentacao/apresentacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ApresentacaoComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
